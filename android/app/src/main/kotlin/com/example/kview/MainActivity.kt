996f58ae0c45a56194dcdb02bccdc5d5977cd530
Raghav Tandon <raghavtan@gmail.com>
2023-06-23 13:02:01 +0200

    application init

---
+++ android/app/src/main/kotlin/com/example/kview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:yaml/yaml.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  var result = await Process.run('kubectl', ['config','view']);
+  var kubeContext = loadYaml(result.stdout)['contexts'];
+  print(kubeContext);
+
+  runApp(const Kview());
+}
+
+
+class Kview extends StatelessWidget {
+  const Kview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Kubernetes View',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
